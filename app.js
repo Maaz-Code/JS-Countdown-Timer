32e3a547cb8f2ef9fb906fcf03dace74593ca41a
Maaz Ahmed <65590762+Maaz-Code@users.noreply.github.com>
2021-01-08 22:06:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var countdownDate = new Date("Jan 31, 2021 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    var gap = countdownDate - now;
+
+    var days = Math.floor(gap / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((gap % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = days + "d " + ": " + hours + "h "+ ": " + minutes + "m " + ": " + seconds + "s ";
+
+    if (gap < 0) {
+        document.getElementById("timer").innerHTML = "!Ended!";
+        clearInterval(x);
+    }
+},500);
